62bded2dd7da29cce3968eabc12df96255dbbc90
nhongprosl <nhongpro8@gmail.com>
2023-11-22 20:33:09 +0700

    init project
    init project

---
+++ iOSBase/iOSBase/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOSBase
+//
+//  Created by Ky Anh on 22/11/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
